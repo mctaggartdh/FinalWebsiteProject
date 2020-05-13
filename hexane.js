59eced7350a43df4e4779b205483d6cddbb9a24b
mctaggartdh <63888068+mctaggartdh@users.noreply.github.com>
2020-05-13 12:33:38 -0400

    Add floating info box

--- hexane.js
+++ hexane.js
@@ -31,3 +31,38 @@ function mouseOut2() {
     IrDiv.style.visibility = "hidden";
     solIrB.style.opacity = 0;
 }
+
+moreInfo = document.getElementById("moreinfo");
+popUp = document.getElementById("popup");
+Close = document.getElementById("close");
+
+moreInfo.addEventListener("click", infoClick);
+moreInfo.addEventListener("mouseover", infoHover);
+moreInfo.addEventListener("mouseout", infoOut);
+Close.addEventListener("click", closeClick);
+Close.addEventListener("mouseover", closeHover);
+Close.addEventListener("mouseout", closeOut);
+
+function infoClick() {
+    popUp.style.visibility = "visible";
+}
+
+function closeClick() {
+    popUp.style.visibility = "hidden";
+}
+
+function infoHover() {
+    moreInfo.style.color = "rgb(0, 0, 252)";
+}
+
+function infoOut() {
+    moreInfo.style.color = "";
+}
+
+function closeHover() {
+    Close.style.color = "rgb(0, 0, 252)";
+}
+
+function closeOut() {
+    Close.style.color = "";
+}
